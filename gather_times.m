8e9e71163c63fdc6a2a2f793f45291703423135c
Nuno Fachada <faken@fakenmc.com>
2015-11-24 16:39:20 +0000

    Add first basic utils + .gitignore + LICENSE

---
+++ gather_times.m
@@ -0,0 +1,44 @@
+function timings = gather_times(name, folder, files)
+% GATHER_TIMES Load execution times from all files in a given folder
+%
+%   GATHER_TIMES(name, folder, files)
+%
+% Parameters:
+%    name - Name of this list of times
+%  folder - Folder containing files with times (output of GNU time command)
+%   files - Files with times (output of GNU time command), use wildcards
+%
+% Returns:
+%  A struct with fields 'name' and 'elapsed', the former containing the
+%  name of this list of times, the latter containing a vector of times (in
+%  seconds) extracted from the specified files
+%    
+% Copyright (c) 2015 Nuno Fachada
+% Distributed under the MIT License (See accompanying file LICENSE or copy 
+% at http://opensource.org/licenses/MIT)
+%
+
+% Get file list
+listing = dir([folder '/' files]);
+
+% How many files?
+numFiles = size(listing, 1);
+
+% Initialize timing array
+elapsed = zeros(numFiles, 1);
+
+if numFiles == 0
+    error(['No files found: ' folder '/' files]);
+end;
+
+for i = 1:numFiles
+    
+    % Get timing information from current file
+    timing = get_time([folder '/' listing(i).name]);
+    
+    % Gather timing
+    elapsed(i, 1) = timing.elapsed;
+
+end;
+
+timings = struct('name', name, 'elapsed', elapsed);
---
+++ get_time.m
@@ -0,0 +1,44 @@
+function timing = get_time(filename)
+% GET_TIME Given a file containing the default output of the GNU time
+% command, extract the user, system and elapsed time in seconds, as well as
+% the percentage of CPU usage.
+%
+%   GET_TIME(filename)
+%
+% Parameters:
+%   filename - File containing output of GNU time command
+%
+% Returns:
+%  A struct with fields 'user', 'sys', 'elapsed' and 'cpu', containing the
+%  user, system and elapsed time in seconds, and the percentage of CPU 
+%  usage, respectively.
+%    
+% Copyright (c) 2015 Nuno Fachada
+% Distributed under the MIT License (See accompanying file LICENSE or copy 
+% at http://opensource.org/licenses/MIT)
+%
+
+% Open file containing output of the time command
+fid = fopen(filename);
+if fid==-1
+    error(['Unknown file' filename]);
+end;
+
+% Scan time info in seconds from file
+tinfo = textscan(fid, '%fuser %fsystem %[^e]elapsed %d');
+seconds = 0;
+splited = strsplit(tinfo{3}{1}, ':');
+mult = 1;
+for i=numel(splited):-1:1
+    seconds = seconds + str2double(splited(i)) * mult;
+    mult = mult * 60;
+end;
+
+% Create struct to return
+timing = struct('user', tinfo{1}, ...
+    'sys', tinfo{2}, ...
+    'elapsed', seconds, ...
+    'cpu', tinfo{4});
+
+% Close file
+fclose(fid);
