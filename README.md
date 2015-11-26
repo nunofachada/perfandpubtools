6fd71dcc75cffbd0ca5c19ac5db5d33e62976bad
Nuno Fachada <faken@fakenmc.com>
2015-11-26 20:30:44 +0000

    perfstats code improvements + add examples to README

--- README.md
+++ README.md
@@ -27,10 +27,11 @@ time in seconds, as well as the percentage of CPU usage.
 * [gather_times](gather_times.m) - Load execution times from all 
 files in a given folder.
 
-* [scal_plot](scal_plot.m) - Determine mean times and respective standard 
-deviations of computational experiments using folders of files containing the 
-default output of the GNU time command,  optionally plotting a scalability graph
-if different experiments correspond to different computational work sizes
+* [perfstats](perfstats.m) - Determine mean times and respective 
+standard deviations of a computational experiments using folders of 
+files containing the default output of the GNU time command, optionally 
+plotting a scalability graph if different setups correspond to different
+computational work sizes.
 
 ### Examples
 
@@ -40,8 +41,11 @@ Unpack the datasets to any folder and put the complete path to this folder in
 variable `datafolder`, e.g.:
 
 ```matlab
-datafolder = 'path/to/datasets/';
+datafolder = 'path/to/datasets';
 ```
+
+These datasets correspond to the results presented in the manuscript
+"[Parallelization Strategies for Spatial Agent-Based Models](http://arxiv.org/abs/1507.04047)".
 
 #### Example 1. Extract performance data from a file
 
@@ -57,23 +61,85 @@ p = get_time([datafolder 'times/NL/time100v1r1.txt'])
 p.cpu
 ```
 
-#### Example 2. Extract execution lengths from files in a folder
+#### Example 2. Extract execution times from files in a folder
 
-* Extract execution lengths from files
-
-```matlab
-exec_len = gather_times('NetLogo', [datafolder 'times/NL'], 'time100v1*.txt')
-```
-
-* See execution lengths
+* Extract execution times from files
 
 ```matlab
-exec_len.elapsed
+exec_time = gather_times('NetLogo', [datafolder '/times/NL'], 'time100v1*.txt')
 ```
 
-#### Example 3. Get average execution lengths and standard deviations from a number of simulation runs
+* See execution times
 
+```matlab
+exec_time.elapsed
+```
 
+#### Example 3. Average execution times and standard deviations
 
+* Get average execution times and standard deviations from ten 
+simulation runs of the Java implementation of PPHPC (single-thread, ST)
+for size 800, parameter set 2.
+
+```matlab
+st800v2 = struct('sname', '800v1', 'folder', [datafolder '/times/ST'], 'files', 't*800v2*.txt');
+[avg_time, std_time] = perfstats(0, 'ST', {st800v2})
+```
+
+#### Example 4. Compare multiple setups within the same implementation
+
+* For the same PPHPC implementation (Java ST), compare performance for
+sizes 100, 200, 400, 800 and 1600.
+
+```matlab
+st100v2 = struct('sname', '100v2', 'folder', [datafolder '/times/ST'], 'files', 't*100v2*.txt');
+st200v2 = struct('sname', '200v2', 'folder', [datafolder '/times/ST'], 'files', 't*200v2*.txt');
+st400v2 = struct('sname', '400v2', 'folder', [datafolder '/times/ST'], 'files', 't*400v2*.txt');
+st800v2 = struct('sname', '800v2', 'folder', [datafolder '/times/ST'], 'files', 't*800v2*.txt');
+st1600v2 = struct('sname', '1600v2', 'folder', [datafolder '/times/ST'], 'files', 't*1600v2*.txt');
+avg_time = perfstats(0, 'ST', {st100v2, st200v2, st400v2, st800v2, st1600v2})
+```
+
+#### Example 5. Same as previous, with a plot
+
+* Compare performance of the Java ST implementation for sizes 100, 200, 
+400, 800 and 1600, and plot a scalability graph.
+
+* In this case, implementation specs must specify a computational size
+and `perfstats` must be called with 1 instead of 0 in the first 
+argument.
+
+```matlab
+st100v2 = struct('sname', '100v2', 'csize', 100, 'folder', [datafolder '/times/ST'], 'files', 't*100v2*.txt');
+st200v2 = struct('sname', '200v2', 'csize', 200, 'folder', [datafolder '/times/ST'], 'files', 't*200v2*.txt');
+st400v2 = struct('sname', '400v2', 'csize', 400, 'folder', [datafolder '/times/ST'], 'files', 't*400v2*.txt');
+st800v2 = struct('sname', '800v2', 'csize', 800, 'folder', [datafolder '/times/ST'], 'files', 't*800v2*.txt');
+st1600v2 = struct('sname', '1600v2', 'csize', 1600, 'folder', [datafolder '/times/ST'], 'files', 't*1600v2*.txt');
+perfstats(1, 'ST', {st100v2, st200v2, st400v2, st800v2, st1600v2})
+```
+
+#### Example 6. Compare different implementations
+
+* Compare NetLogo (NL) and Java single-thread (ST) PPHPC implementations
+for sizes 100 to 1600, parameter set 1.
+
+```matlab
+% Specify NetLogo implementation specs
+nl100v1 = struct('sname', '100v1', 'csize', 100, 'folder', [datafolder '/times/NL'], 'files', 't*100v1*.txt');
+nl200v1 = struct('sname', '200v1', 'csize', 200, 'folder', [datafolder '/times/NL'], 'files', 't*200v1*.txt');
+nl400v1 = struct('sname', '400v1', 'csize', 400, 'folder', [datafolder '/times/NL'], 'files', 't*400v1*.txt');
+nl800v1 = struct('sname', '800v1', 'csize', 800, 'folder', [datafolder '/times/NL'], 'files', 't*800v1*.txt');
+nl1600v1 = struct('sname', '1600v1', 'csize', 1600, 'folder', [datafolder '/times/NL'], 'files', 't*1600v1*.txt');
+nlv1 = {nl100v1, nl200v1, nl400v1, nl800v1, nl1600v1};
+% Specify Java ST implementation specs
+st100v1 = struct('sname', '100v1', 'csize', 100, 'folder', [datafolder '/times/ST'], 'files', 't*100v1*.txt');
+st200v1 = struct('sname', '200v1', 'csize', 200, 'folder', [datafolder '/times/ST'], 'files', 't*200v1*.txt');
+st400v1 = struct('sname', '400v1', 'csize', 400, 'folder', [datafolder '/times/ST'], 'files', 't*400v1*.txt');
+st800v1 = struct('sname', '800v1', 'csize', 800, 'folder', [datafolder '/times/ST'], 'files', 't*800v1*.txt');
+st1600v1 = struct('sname', '1600v1', 'csize', 1600, 'folder', [datafolder '/times/ST'], 'files', 't*1600v1*.txt');
+stv1 = {st100v1, st200v1, st400v1, st800v1, st1600v1};
+perfstats(1, 'NL', nlv1, 'ST', stv1)
+```
 
 [GNU time]: https://www.gnu.org/software/time/
+
---
+++ perfstats.m
@@ -0,0 +1,227 @@
+function [times, std_times, times_raw, fid, impl_legend, set_legend] ...
+    = perfstats(do_plot, varargin)
+% PERFSTATS Determine mean times and respective standard deviations of a
+% computational experiment using folders of files containing the default
+% output of the GNU time command, optionally plotting a scalability graph 
+% if different setups correspond to different computational work sizes
+%
+% [times, std_times, times_raw, fids] = PERFSTATS(do_plot, varargin)
+%
+% Parameters:
+%    do_plot - Draw scalability graph (1, 0)?
+%   varargin - Pairs of implementation name and implementation specs. An
+%              implementation name is simply a string specifying the name
+%              of an implementation. An implementation spec is a cell array
+%              where each cell contains a struct with three fields:
+%                 sname - Name of setup, e.g. of series of runs with a 
+%                         given parameter set
+%                folder - Folder containing GNU time output files
+%                files  - Time output file names (use wildcards)
+%                 csize - Computational size associated with setup (can be
+%                         ignored if a plot was not requested)
+%              
+% Output:
+%        times - Matrix of average computational times where each row 
+%                corresponds to an implementation and each column to a 
+%                setup.
+%    std_times - Matrix of the sample standard deviation of the 
+%                computational times. Each row corresponds to an 
+%                implementation and each column to a setup.
+%    times_raw - Cell matrix where each cell contais a complete time struct 
+%                for each setup. Rows correspond to implementations,
+%                columns to setups.
+%          fid - ID of generated plot (if doPlot == 1).
+%  impl_legend - Implementations legend.
+%   set_legend - Setups legend.
+%
+%    
+% Copyright (c) 2015 Nuno Fachada
+% Distributed under the MIT License (See accompanying file LICENSE or copy 
+% at http://opensource.org/licenses/MIT)
+%
+
+fid = 0;
+
+% Check arguments and obtain number of implementations and setups
+[nimpl, nset, set_legend, set_sizes] = check_args(varargin);
+
+% Initialize output variables
+times = zeros(nimpl, nset);
+std_times = zeros(nimpl, nset);
+times_raw = cell(nimpl, nset);
+
+% Initialize legends
+impl_legend = cell(nimpl, 1);
+
+% Determine mean time and sample standard deviation for all implementations
+% and setups
+for i = 1:nimpl
+    
+    % Get name of current implementation
+    impl_name = varargin{(i - 1) * 2 + 1};
+    
+    % Get specs of current implementation
+    ispecs = varargin{(i - 1) * 2 + 2};
+
+    for j = 1:nset
+
+        % Get data for current setup
+        sdata = ispecs{j};
+    
+        % Determine mean time and sample standard deviation for current
+        % implementation
+        times_raw{i, j} = gather_times([impl_name '-' sdata.sname], ...
+            sdata.folder, sdata.files);
+        times(i, j) = mean(times_raw{i, j}.elapsed);
+        std_times(i, j) = std(times_raw{i, j}.elapsed);
+
+    end;
+    
+    % Compose implementations legend
+    impl_legend{i} = impl_name;
+
+end;
+
+% Draw scalability plot, if required
+if do_plot
+    
+    if numel(unique(set_sizes)) ~= nset
+        error(['Can''t plot if different setups within an ' ...
+            'implementation have equal computational sizes.']);
+    end;
+    
+    if nset == 1
+        warning('Can''t plot with only one setup per implementation.');
+    else
+        
+        % Adjust for weird plotting behavior if number of implementations
+        % is the same as the number of setups within each implementation
+        if numel(set_sizes) == numel(impl_legend)
+            times_to_plot = times';
+        else
+            times_to_plot = times;
+        end;        
+
+        % Draw figure
+        fid = figure();
+        loglog(set_sizes, times_to_plot, '-o');
+        grid on;
+        legend(impl_legend, 'Location', 'NorthWest');
+        set(gca,'XTick', set_sizes);
+        xlim([min(set_sizes) max(set_sizes)]);
+        xlabel('Size');
+        ylabel('Time (s)');
+        
+    end;
+end;
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
+% Helper function to check args %
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
+function [nimpl, nset, set_legend, set_sizes] = check_args(args)
+
+% There must be a pair number of variable arguments
+if mod(numel(args), 2) > 0
+    error(['Variable arguments must be pairs of implementation name '...
+        'and implementation spec (struct or cell)']);
+end;    
+
+% Determine number of implementations and setups
+nimpl = numel(args) / 2;
+nset = numel(args{2});
+
+% Initialize setup legends
+set_legend = cell(nset, 1);
+
+% Initialize computational work sizes of setups for plotting, if necessary
+set_sizes = zeros(nset, 1);
+
+% Check if implementation names and specs are valid
+for i=1:2:numel(args)
+    
+    % Get implementation name
+    arg_name = args{i};
+    
+    % Get implementation spec
+    arg_spec = args{i + 1};
+    
+    % Check if implementation name is a string
+    if ~ischar(arg_name)
+        error(['The ' num2str(i) ' parameter should be a string '...
+            'with an implementation name']);
+    end;
+    
+    % Check if implementation spec is a cell
+    if ~iscell(arg_spec)
+        error(['The ' num2str(i + 1) ' parameter should be a cell ' ...
+            'containing the implementation spec']);
+    end;
+
+    % Check if it contains the same number of setups.
+    if nset ~= numel(arg_spec)
+        error(['All implementations must have the same number of '...
+            'setups']);
+    end;
+        
+    % Check if each element of the cell is a properly formed struct.
+    for j=1:numel(arg_spec)
+
+        % Does struct has the required fields?
+        if ~isfield(arg_spec{j}, 'sname') || ...
+            ~isfield(arg_spec{j}, 'folder') ||  ...
+            ~isfield(arg_spec{j}, 'files')
+
+            error(['Variable parameters must be structs with fields '...
+                '"setup", "folder" and "files"']);
+
+        end;
+        
+        % Is this the first implementation?
+        if i == 1
+
+            % If so, keep information about its setups
+            
+            % Compose setup legends
+            set_legend{j} = arg_spec{j}.sname;
+
+            % Compose setup sizes
+            if isfield(arg_spec{j}, 'csize')
+                set_sizes(j) = arg_spec{j}.csize;
+                if j > 1
+                    if set_sizes(j - 1) >= set_sizes(j)
+                        error(['Setups within an implementation ' ...
+                            'must be ordered by ascending ' ...
+                            'computational size']);
+                    end;
+                end;
+            end;
+
+        else
+            
+            % Otherwise check that setups from posterior implementations
+            % have the same name and computational size as the ones in the
+            % first implementation
+
+            % Check legends
+            if ~strcmp(set_legend{j}, arg_spec{j}.sname)
+                error(['Different implementations have associated ' ...
+                    'setups with different names']);
+            end;
+            
+            % Check computational sizes
+            set_sizes_aux = 0;
+            if isfield(arg_spec{j}, 'csize')
+                set_sizes_aux = arg_spec{j}.csize;
+            end;
+            if set_sizes(j) ~= set_sizes_aux
+                error(['Different implementations have associated ' ...
+                    'setups with different computational sizes']);
+
+            end;
+
+        end;
+    end;
+    
+end;
+
+
